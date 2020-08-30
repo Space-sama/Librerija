38316821f723320169d39a564a88d7117ba207c5
Oussama <oussama.elharmali@gmail.com>
2020-08-31 00:02:32 +0200

    initial setup

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+
+    res.render('index');
+
+})
+
+router.get('/Oussama', (req, res) => {
+
+    res.render('Oussama');
+
+})
+
+
+
+
+module.exports = router;
