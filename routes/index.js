66ea1fcb2ac5ecf0c95ac29dccafefece89b9086
Oussama <oussama.elharmali@gmail.com>
2020-08-31 04:46:48 +0200

    initial Author Routes

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,6 @@ router.get('/Oussama', (req, res) => {
 
 
 
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -6,15 +6,20 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
 
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 // Database
 const mongoose = require('mongoose');
@@ -26,6 +31,7 @@ db.once('open', () => console.log("Connected to mongoose successfully."));
 
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
 
